354d468001dd7823fccedb7bacd105d7636b169e
MFHava <mfh@live.at>
2023-04-18 13:01:31 +0200

    added test with custom allocator

--- test/tls.cpp
+++ test/tls.cpp
@@ -5,6 +5,8 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #include <future>
+#include <algorithm>
+#include <execution>
 #include <catch.hpp>
 #include <tls.hpp>
 
@@ -24,7 +26,62 @@ namespace {
 		~move_only() noexcept =default;
 	};
 
-	//TODO: test with custom allocator
+
+	class allocator_state final {
+		std::mutex m;
+		std::byte * pool;
+		std::size_t offset{0};
+	public:
+		explicit
+		allocator_state(std::size_t size) : pool{reinterpret_cast<std::byte *>(std::malloc(size))} {
+			if(!pool) throw std::bad_alloc{};
+		}
+
+		allocator_state(const allocator_state &) =delete;
+		auto operator=(const allocator_state &) -> allocator_state & =delete;
+
+		~allocator_state() noexcept { std::free(pool); }
+
+		template<typename T>
+		auto allocate(std::size_t n) -> T * {
+			if(!n) return nullptr;
+
+			const std::lock_guard lock{m};
+			auto ptr{reinterpret_cast<T *>(pool + offset)};
+			offset += n * sizeof(T);
+			return ptr;
+		}
+
+		template<typename T>
+		void deallocate(T *, std::size_t) noexcept {
+			//nop ... only dtor releases memory
+		}
+	};
+
+	template<typename T>
+	class allocator final {
+		template<typename U>
+		friend
+		class allocator;
+
+		std::shared_ptr<allocator_state> state;
+	public:
+		using value_type = T;
+
+		explicit
+		allocator(std::size_t size) : state{std::make_shared<allocator_state>(sizeof(T) * size)} {}
+
+		template<typename U>
+		requires (not std::same_as<T, U>)
+		allocator(const allocator<U> & other) noexcept : state{other.state} {}
+
+		auto allocate(std::size_t n) -> T * { return state->template allocate<T>(n); }
+
+		void deallocate(T * ptr, std::size_t n) noexcept { state->deallocate(ptr, n); }
+
+		friend
+		auto operator==(const allocator & lhs, const allocator & rhs) noexcept -> bool =default;
+	};
 }
 
 TEST_CASE("tls default ctor", "[tls] [ctor] [default]") {
@@ -74,4 +131,17 @@ TEST_CASE("tls clear", "[tls]") {
 	tls.clear();
 	REQUIRE(std::get<1>(tls.local()));
 	REQUIRE(!std::get<1>(tls.local()));
+}
+
+TEST_CASE("tls custom allocator", "[tls] [allocator]") {
+	std::vector<int> vec(1'000'000);
+	std::iota(std::begin(vec), std::end(vec), 0);
+
+
+	p2774::tls<int, allocator<int>> tls{allocator<int>{1'024}};
+	std::for_each(std::execution::par, std::begin(vec), std::end(vec), [&](const auto &) {
+		auto [local, _]{tls.local()};
+	});
+
+	REQUIRE(std::distance(tls.begin(), tls.end()));
 }
